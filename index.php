141d05fb03441573ca5c4728bb1dcc877f889935
x-dcx-x <petrisot.guillaume@gmail.com>
2023-02-14 14:22:22 +0100

    commit initial

--- index.php
+++ index.php
@@ -4,44 +4,52 @@
 $a = 50;
 $b = 10;
 
-       >    {
+if (!($a <= $b)) {
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
+
+if ($a !== $b) {
     echo "<br><br>Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+
+if ($a === $b) {
     echo "<br><br>Oui";
 }
 
-{
+if ($a > $b) {
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
-echo"<br><br>";
- ($a == $b) {
+echo "<br><br>";
+if ($a == $b) {
     echo "1";
-}  ($a > $b) {
+}
+if ($a > $b) {
     echo "2";
 }
-{
+if ($a < $b) {
     echo "3";
 }
 
 
 //Cinquieme ligne
-echo"<br><br>";
-
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
-    echo "Welcome";
+echo "<br><br>";
+$color = "red";
+if ($color = "red" || "welcome") {
+    echo "Hello" . "<br>";
+    $color = "green";
+    if ($color = "green")
+        echo "Welcome" . "<br>";
+    $result = null;
+    $result = "valeur Null";
+    if ($color = "red" || "green") {
+        echo $result;
+    }
 }
 
